fix bug:Fixed incorrect results when activation values are all negative

src/models/lorsa.py
@@ -358,36 +358,7 @@ class LowRankSparseAttention(nn.Module):
             top_k_values, _ = torch.kthvalue(l1, k=k_smallest, dim=2)
             
             # top_k_mask: (batch_size, query_pos, n_heads)
-            top_k_mask = l1 >= top_k_values.unsqueeze(-1)
-
-            '''
-            # batch topk
-            k_smallest = (self.cfg.n_ov_heads - self.cfg.top_k) * filter_mask.numel() + 1
-
-            top_k_values, _ = torch.kthvalue(l1.contiguous().view(-1), k=k_smallest)
-
-            top_k_mask = l1 >= top_k_values
-            
-            # approximate batch topk
-            k_smallest = self.cfg.n_ov_heads - self.cfg.top_k + 1
-
-            # top_k_values: (batch_size, query_pos)
-            top_k_values, _ = torch.kthvalue(l1, k=k_smallest, dim=2)
-            
-            # top_k_mask: (batch_size, query_pos, n_heads)
-            top_k_mask = l1 >= top_k_values[filter_mask].median()
-
-            # sentence topk
-            k_smallest = filter_mask.sum(dim=-1) * (self.cfg.n_ov_heads - self.cfg.top_k) + 1
-
-            l1[~filter_mask] = float('-inf')
-
-            flat_l1 = l1.contiguous().view(l1.shape[0], -1)
-
-            top_k_values = torch.stack([flat_l1[i].kthvalue(k_smallest[i]).values for i in range(l1.shape[0])])
-
-            top_k_mask = l1 >= top_k_values.view(-1, 1, 1)
-            '''
+            top_k_mask = l1 > top_k_values.unsqueeze(-1)
 
         top_k_z = z * top_k_mask.unsqueeze(-1)
 
@@ -482,35 +453,6 @@ class LowRankSparseAttention(nn.Module):
         x_rotated = x_rot * rotary_cos + x_flip * rotary_sin
 
         return torch.cat([x_rotated, x_pass], dim=-1)
-
-    '''
-    def calculate_sin_cos_rotary(
-        self,
-        rotary_dim: int,
-        n_ctx: int,
-        base: int = 10000,
-        dtype: torch.dtype = torch.float32,
-    ) -> Tuple[Float[torch.Tensor, "n_ctx rotary_dim"], Float[torch.Tensor, "n_ctx rotary_dim"]]:
-        """
-        Calculate the sine and cosine waves to use in a rotary embedding. See https://blog.eleuther.ai/rotary-embeddings/ for details
-
-        Note: For some inexplicable reason, in GPT-J each ADJACENT pair of elements in k and q are rotated, in GPT-NeoX the pair of elements at k and k+n//2 are rotated (ie folding the full length in half, and then looking at pairs accordingly). I have absolutely no clue why, it should be completely equivalent.
-        To resolve this, I've coded it to default to the GPT-J mode, but to explicitly check whether it's GPT-NeoX and then do the GPT-NeoX thing if it is.
-        """
-        high_precision = torch.float32 if dtype != torch.float64 else torch.float64
-        pos = torch.arange(n_ctx, dtype=high_precision)
-        dim = torch.arange(rotary_dim // 2, dtype=high_precision)
-
-        # A set of frequencies evenly spaced in log space
-        freq = base ** (dim / (rotary_dim / 2))
-        if self.cfg.rotary_adjacent_pairs:
-            freq = einops.repeat(freq, "d -> (d 2)")
-        else:
-            freq = einops.repeat(freq, "d -> (2 d)")
-        # Create a n_ctx x rotary_dim tensor, where each column is an arithmetic sequence of angles in that frequency
-        angles = pos[:, None] / freq[None, :]
-        return torch.sin(angles).to(dtype), torch.cos(angles).to(dtype)
-    '''
 
     def calculate_sin_cos_rotary(
         self,

src/runner.py
@@ -12,36 +12,38 @@ from datasets import load_from_disk
 
 from models.lorsa import LowRankSparseAttention
 from config import LorsaTrainConfig, LorsaAnalyzeConfig, DataGenConfig
-from train import train_lorsa
+from train import train_lorsa, debug_train_lorsa
 from activations import TextActivationDataset, PresaveActivationDataset
 import wandb
 
 import copy
 
 def train_lorsa_runner(cfg: LorsaTrainConfig):
-    # load model
-    hf_model = AutoModelForCausalLM.from_pretrained(
-        cfg.model, 
-        local_files_only=True
-    )
-    tokenizer = AutoTokenizer.from_pretrained(
-        cfg.model, 
-        local_files_only=True
-    )
+    if cfg.dataset_type == 'text' or cfg.init_qk_with_orig_qk == True:
+        # load model
+        hf_model = AutoModelForCausalLM.from_pretrained(
+            cfg.model,
+            local_files_only=True
+        )
+        tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(
+            cfg.model, 
+            local_files_only=True
+        )
 
-    model = HookedTransformer.from_pretrained_no_processing(
-        cfg.model_name, 
-        use_flash_attn=True, 
-        hf_model=hf_model,
-        hf_config=hf_model.config,
-        tokenizer=tokenizer,
-        device=cfg.lorsa_config.device,
-        dtype=cfg.lorsa_config.dtype,
-    )
-    model.offload_params_after(f'blocks.{cfg.layer}.hook_attn_out', torch.tensor([[0]], device=cfg.lorsa_config.device))
-    model.eval()
-    for param in model.parameters():
-        param.requires_grad = False
+        model = HookedTransformer.from_pretrained_no_processing(
+            cfg.model_name, 
+            use_flash_attn=True, 
+            hf_model=hf_model,
+            hf_config=hf_model.config,
+            tokenizer=tokenizer,
+            device=cfg.lorsa_config.device,
+            dtype=cfg.lorsa_config.dtype,
+        )
+        model.offload_params_after(f'blocks.{cfg.layer}.hook_attn_out', torch.tensor([[0]], device=cfg.lorsa_config.device))
+        model.eval()
+        for param in model.parameters():
+            param.requires_grad = False
+            
     # load activation dataset
     if cfg.dataset_type == 'text':
         activation_dataset = TextActivationDataset(cfg=cfg, model=model, tokenizer=tokenizer)
@@ -54,11 +56,10 @@ def train_lorsa_runner(cfg: LorsaTrainConfig):
 
     cfg.lorsa_config.save_config(save_path=cfg.result_dir)
 
-    orig_attn = model.blocks[cfg.layer].attn
-    
     lorsa = LowRankSparseAttention(cfg.lorsa_config).to(cfg.lorsa_config.device, non_blocking=True)
 
     if cfg.init_qk_with_orig_qk == True:
+        orig_attn = model.blocks[cfg.layer].attn
         lorsa.initialize_parameters(
             W_Q = orig_attn.W_Q.repeat(lorsa.cfg.n_qk_heads // orig_attn.cfg.n_heads, 1, 1) * lorsa.cfg.avg_norm['in'] / math.sqrt(lorsa.cfg.d_model),
             b_Q = orig_attn.b_Q.repeat(lorsa.cfg.n_qk_heads // orig_attn.cfg.n_heads, 1),
@@ -71,8 +72,6 @@ def train_lorsa_runner(cfg: LorsaTrainConfig):
             lorsa.set_requires_grad('b_Q', requires_grad=False)
             lorsa.set_requires_grad('W_K', requires_grad=False)
             lorsa.set_requires_grad('b_K', requires_grad=False)
-
-    # lorsa.initialize_parameters(b_O = orig_attn.b_O.data.clone().detach().to(cfg.lorsa_config.dtype) * math.sqrt(cfg.lorsa_config.d_model) / activation_dataset.avg_norm['out'])
 
     # init wandb
     if cfg.log_to_wandb:
@@ -99,24 +98,85 @@ def train_lorsa_runner(cfg: LorsaTrainConfig):
     )
     
     # save
-    
     torch.save(lorsa.state_dict(), os.path.join(cfg.result_dir, f"final.pth"))
     
     # finish wandb
     if cfg.log_to_wandb:
         wandb.finish()
 
-@torch.no_grad()
-def analyze_lorsa_runner(cfg: LorsaAnalyzeConfig):
-    if cfg.dataset_type == 'text':
-        hf_model = GPTNeoXForCausalLM.from_pretrained(cfg.model_name)
-        tokenizer = GPTNeoXTokenizerFast.from_pretrained(cfg.model_name)
-        model = HookedTransformer.from_pretrained_no_processing(cfg.model_name, use_flash_attn=True, hf_model=hf_model, tokenizer=tokenizer, device=cfg.lorsa_config.device, dtype=cfg.lorsa_config.dtype)
+def debug_train_lorsa_runner(cfg: LorsaTrainConfig):
+    if cfg.dataset_type == 'text' or cfg.init_qk_with_orig_qk == True:
+        # load model
+        hf_model = AutoModelForCausalLM.from_pretrained(
+            cfg.model,
+            local_files_only=True
+        )
+        tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(
+            cfg.model, 
+            local_files_only=True
+        )
+
+        model = HookedTransformer.from_pretrained_no_processing(
+            cfg.model_name, 
+            use_flash_attn=True, 
+            hf_model=hf_model,
+            hf_config=hf_model.config,
+            tokenizer=tokenizer,
+            device=cfg.lorsa_config.device,
+            dtype=cfg.lorsa_config.dtype,
+        )
         model.offload_params_after(f'blocks.{cfg.layer}.hook_attn_out', torch.tensor([[0]], device=cfg.lorsa_config.device))
         model.eval()
-        activation_dataset = TextActivationDataset(cfg=cfg, model=model)
+        for param in model.parameters():
+            param.requires_grad = False
+            
+    # load activation dataset
+    if cfg.dataset_type == 'text':
+        activation_dataset = TextActivationDataset(cfg=cfg, model=model, tokenizer=tokenizer)
     elif cfg.dataset_type == 'activation':
         activation_dataset = PresaveActivationDataset(cfg=cfg)
     else:
         raise ValueError(f"Unknown dataset type: {cfg.dataset_type}")
+    
+    cfg.lorsa_config.avg_norm = activation_dataset.cal_norm()
 
+    lorsa = LowRankSparseAttention(cfg.lorsa_config).to(cfg.lorsa_config.device, non_blocking=True)
+
+    if cfg.init_qk_with_orig_qk == True:
+        orig_attn = model.blocks[cfg.layer].attn
+        lorsa.initialize_parameters(
+            W_Q = orig_attn.W_Q.repeat(lorsa.cfg.n_qk_heads // orig_attn.cfg.n_heads, 1, 1) * lorsa.cfg.avg_norm['in'] / math.sqrt(lorsa.cfg.d_model),
+            b_Q = orig_attn.b_Q.repeat(lorsa.cfg.n_qk_heads // orig_attn.cfg.n_heads, 1),
+            W_K = orig_attn.W_K.repeat(lorsa.cfg.n_qk_heads // orig_attn.cfg.n_heads, 1, 1) * lorsa.cfg.avg_norm['in'] / math.sqrt(lorsa.cfg.d_model),
+            b_K = orig_attn.b_K.repeat(lorsa.cfg.n_qk_heads // orig_attn.cfg.n_heads, 1),
+        )
+        print('init lorsa qk parameters using orig qk')
+        if cfg.fix_qk == True:
+            lorsa.set_requires_grad('W_Q', requires_grad=False)
+            lorsa.set_requires_grad('b_Q', requires_grad=False)
+            lorsa.set_requires_grad('W_K', requires_grad=False)
+            lorsa.set_requires_grad('b_K', requires_grad=False)
+
+    # init wandb
+    if cfg.log_to_wandb:
+        config = copy.deepcopy(cfg)
+        config.lorsa_config.dtype = str(config.lorsa_config.dtype)
+        wandb.init(
+            project=cfg.wandb_project,
+            config=config,
+            name=cfg.project_name,
+            entity=cfg.wandb_entity,
+            mode=os.getenv('WANDB_MODE', 'online'),
+            # save_code=False,
+            # ync_tensorboard=False,
+            # settings=wandb.Settings(
+            #     _disable_stats=True
+            # )
+        )
+        
+    # train lorsa
+    lorsa = debug_train_lorsa(
+        lorsa=lorsa,
+        cfg=cfg,
+        activation_dataset=activation_dataset,
+    )